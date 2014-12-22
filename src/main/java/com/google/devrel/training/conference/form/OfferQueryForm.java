4e6869fbb9eac664fb9116871b880af8ae8a35c4
karthik kandasamy <karthik.kandasamy@gmail.com>
2014-12-22 18:21:45 -0500

    Revert "added sort, dropped lat/lng in offer"
    This reverts commit 70868994c5afb2fce941a4a9289147eeab561d5b.

--- src/main/java/com/google/devrel/training/conference/form/OfferQueryForm.java
+++ src/main/java/com/google/devrel/training/conference/form/OfferQueryForm.java
@@ -9,7 +9,6 @@ import java.util.logging.Logger;
 
 import com.google.api.server.spi.config.AnnotationBoolean;
 import com.google.api.server.spi.config.ApiResourceProperty;
-import com.google.devrel.training.conference.form.OfferForm.Cuisine;
 
 /**
  * A simple Java object (POJO) representing a query options for Offer.
@@ -23,8 +22,7 @@ public class OfferQueryForm {
 	private float longitude;
 	private Date offerDate = new Date();
 
-	private Cuisine cuisine;
-
+	// private final Cuisine cuisine;
 	private int distanceInKm = 10; // default to 10 km
 	// private final SortField sortField;
 
@@ -34,15 +32,16 @@ public class OfferQueryForm {
 	@ApiResourceProperty(ignored = AnnotationBoolean.TRUE)
 	private static final Format sdf = new SimpleDateFormat("YYYY-MM-dd");
 
-	private SortField sortField = SortField.DISTANCE;
-
 	// default constructor
 	public OfferQueryForm() {
 	}
 
 	public OfferQueryForm(final float latitude, final float longitude,
-			final int distanceInKm, final Date offerDate,
-			final Cuisine cuisine, final SortField sortField) {
+			final int distanceInKm, final Date offerDate /*
+														 * , final Cuisine
+														 * cuisine, final
+														 * SortField sortField
+														 */) {
 
 		// default sort by distance - ascending order
 
@@ -53,15 +52,14 @@ public class OfferQueryForm {
 			this.offerDate = offerDate;
 		}
 
-		this.cuisine = cuisine;
+		// this.cuisine = cuisine;
 
 		if (distanceInKm > 0) {
 			this.distanceInKm = distanceInKm;
 		}
 
-		if (sortField != null) {
-			this.sortField = sortField;
-		}
+		// this.sortField = sortField;
+
 	}
 
 	public float getLatitude() {
@@ -76,17 +74,17 @@ public class OfferQueryForm {
 		return this.offerDate;
 	}
 
-	public Cuisine getCuisine() {
-		return this.cuisine;
-	}
+	// public Cuisine getCuisine() {
+	// return this.cuisine;
+	// }
 
 	public int getDistanceInKm() {
 		return this.distanceInKm;
 	}
 
-	public SortField getSortField() {
-		return this.sortField;
-	}
+	// public SortField getSortField() {
+	// return this.sortField;
+	// }
 
 	@ApiResourceProperty(ignored = AnnotationBoolean.TRUE)
 	public float getMinLatitude() {
